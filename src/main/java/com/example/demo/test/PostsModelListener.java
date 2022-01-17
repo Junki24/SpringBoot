c82f795c5398ea6da68d3476d6751eb100010477
koko7 <wnsrl315250>
2022-01-17 23:15:06 +0900

    commit test

--- src/main/java/com/example/demo/test/PostsModelListener.java
+++ src/main/java/com/example/demo/test/PostsModelListener.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class PostsModelListener extends AbstractMongoEventListener<Posts> {
 
+
     private final SequenceGeneratorService sequenceGeneratorService;
 
     @Override
